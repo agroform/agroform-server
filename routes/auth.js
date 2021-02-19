5e46c33b6c9522fe4e1af609b0068f5559b38231
Zhenni Qian <zhenni.qian913@gmail.com>
2021-02-19 22:46:17 +0100

    user auth routes API done

--- routes/auth.js
+++ routes/auth.js
@@ -68,6 +68,53 @@ router.post('/register', (req, res, next) => {
 
 });
 
+router.post('/login', (req, res, next) => {
+  passport.authenticate('local', (err, theUser, failureDetails) => {
+      if (err) {
+          res
+            .status(500)
+            .json({ 
+              message: "Something went wrong authenticating user" 
+            });
+          return;
+      }
+  
+      if (!theUser) {
+          res
+            .status(401)
+            .json(failureDetails);
+          return;
+      }
 
+      req.login(theUser, (err) => {
+          if (err) {
+              res
+                .status(500)
+                .json({ 
+                  message: "Session save went wrong"
+                });
+              return;
+          }
+
+          // We are now logged in (that's why we can also send req.user)
+          res.status(200).json(theUser);
+      });
+  })
+  (req, res, next);
+});
+
+router.post('/logout', (req, res, next) => {
+  req.logout();
+  res.status(200).json({ message: "Successfully logged out" });
+});
+
+
+router.get('/loggedin', (req, res, next) => {
+  if (req.isAuthenticated()) {
+      res.status(200).json(req.user);
+      return;
+  }
+  res.status(403).json({ message: 'Unauthorized' });
+});
 
 module.exports = router;
